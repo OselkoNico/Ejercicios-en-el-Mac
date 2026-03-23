22c38cb37c29b8c0a40790aa7cc41a14cc685bac
Osel Nicolás <oselnicolas@MacBook-Pro-de-Osel.local>
2026-03-23 10:22:00 +0100

    Ejercicio 2 con test

---
+++ Ejercicios_JS/ejer2.js
@@ -0,0 +1,7 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+
+function mayorQue(arr){
+    return arr.filter(numero => numero>5)
+};
+
+module.exports = mayorQue;
---
+++ Ejercicios_JS/ejer2.test.js
@@ -0,0 +1,5 @@
+const mayorQue = require('./ejer2');
+
+test('Función de array mayor que 5', () => {
+    expect(mayorQue([2, 3, 4, 5, 6])).toEqual([6])
+});
