b9a41663f1700f9cf853cee710f59733f2bf9225
Osel Nicolás <oselko123@gmail.com>
2026-03-26 15:04:44 +0100

    Ejercicio 3 practicando cypress

--- practica2.js
+++ practica2.js
@@ -1,9 +1,16 @@
-describe('Comprar en una tienda', () => {
-    it('Comprar pan', () => {
+describe('Ejercicio de añadir varias tareas', () => {
+    it('Comprar pan y leche', () => {
         cy.visit('http://www.ejemplo.com')
+
         cy.get('#tarea').type('Comprar pan')
         cy.contains('button', 'Añadir').click()
+
+        cy.get('#tarea').type('Comprar leche')
+        cy.contains('button', 'Añadir').click()
+
         cy.get('#lista').should('contain', 'Comprar pan')
+        cy.get('#lista').should('contain', 'Comprar leche')
+
         cy.get('#tarea').should('have.value', '')
     })
 })
