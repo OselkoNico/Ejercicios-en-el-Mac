4beede55c4a22b167f91ba2f911793073c75dbba
Osel Nicolás <oselko123@gmail.com>
2026-03-26 14:40:40 +0100

    Extension del anterior ejercicio

--- practica.js
+++ practica.js
@@ -1,11 +1,19 @@
 describe('Prueba de formulario', () => {
-    it('Usuario envia formulario', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:5500')
+    })
+    it('Usuario envia formulario', () => {
         cy.get('#nombre').type("Juan")
         cy.get('#email').type("juan@test.com")
         cy.get('#btnEnviar').click()
 
         cy.get('#mensaje')
             .should('contain', 'Formulario enviado correctamente')
+    })
+    it('Usuario sin email, no envia formulario', () => {
+        cy.get('#nombre').type("Juan")
+        cy.get('#btnEnviar').click()
+        cy.get('#mensaje')
+            .should('contain', 'Email obligatorio')
     })
 })
